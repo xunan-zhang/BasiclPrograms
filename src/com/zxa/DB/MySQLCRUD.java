2a3dd1164109f6130052ca2e5352a9ab90b93879
xunan-zhang <xunan-zhang@users.noreply.github.com>
2018-08-28 17:39:38 -0400

    add new database into mysql

--- src/com/zxa/DB/MySQLCRUD.java
+++ src/com/zxa/DB/MySQLCRUD.java
@@ -2,12 +2,15 @@ package com.zxa.DB;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class MySQLCRUD {
 	
 	public static void main(String[] argv) {
-		getMySQLConnection();
+//		getMySQLConnection();
+		search();
 	}
 
 	public static Connection getMySQLConnection() {
@@ -17,7 +20,7 @@ public class MySQLCRUD {
 			String url = "jdbc:mysql://" + 		// db type
 					"localhost:" + 				// host name
 					"3306/" + 					// port
-					"loginuser?" + 				// db name
+					"mydb?" + 				// db name
 					"useSSL=false&" + 			// do not use ssl
 					"user=root&" + 				// login
 					"password=Zxa302059!"; 		// password
@@ -31,5 +34,42 @@ public class MySQLCRUD {
 			throw new RuntimeException(e);
 		}
 	}
+
+	public static Statement getStatement() {
+
+        String url = "jdbc:mysql://localhost:3306/mydb?user=root&password=Zxa302059!";
+		
+        try {
+        	DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+            Connection conn = DriverManager.getConnection(url);
+            Statement st = conn.createStatement();
+
+    		return st;
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+        
+        return null;
+	}
+
+	public static void search() {
+		Statement st = getStatement();
+		try {
+			ResultSet rs = st.executeQuery("SELECT * FROM users");
+
+	        while (rs.next()) {
+	            System.out.print(rs.getInt(1));
+	            System.out.print(", ");
+	            System.out.print(rs.getString(2));
+	            System.out.print(", ");
+	            System.out.println(rs.getString(3));
+	        }
+	        
+	        System.out.println("Complete!");
+		} catch (Exception e) {
+		}	
+        System.out.println("");
+	}
+
 	
 }
